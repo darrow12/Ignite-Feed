ddb83f224b4252a96f3c2543ab86446072f08883
Kim <nicedarrow12@gmail.com>
2022-06-07 19:53:33 -0300

    📘 style: global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './global.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <h1>hello world</h1>
       <Post author="Darrow" content="Hello React world!" />
       <Post author="Jucelino" content="Olá" />
     </>
   )
 }
-
-// JSX = JavaScript + XML
