f6a7190a21c8eadceb1fb38092f282ad1dc3331f
Kim <nicedarrow12@gmail.com>
2022-06-07 20:05:34 -0300

    ✨ feat(component): header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css';
+import igniteLogoImg from '../assets/ignite-logo.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogoImg} alt="Logotipo do Ignite" />
     </header>
   );
 }
